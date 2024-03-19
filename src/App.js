ee3e23ecf506c90b863b49b9726d183b4af0580f
Cihan Danisman <140907366+cihandanisman@users.noreply.github.com>
2024-03-20 00:00:17 +0300

    ImageGallery

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles/style.css";
+import "./helper/data.js";
+import images from "./helper/data.js";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>Image Gallery</h1>
+      </div>
+      <div className="image-container">
+        {images.map((image) => (
+          <div>
+            <div className="main">
+              <img src={image.src.large} alt="" width={"200px"}></img>
+            </div>
+            <h3>{image.photographer}</h3>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
